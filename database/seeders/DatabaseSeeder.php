e22b00c6caa7b1003ae4d0b19a81fb6cf79d75d5
Mykola <melnukkola220904@gmail.com>
2022-10-17 15:13:48 +0300

    refactor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        $this->call([
+            BankSeeder::class,
+            CurrencySeeder::class,
+            RateSeeder::class,
+        ]);
     }
 }
